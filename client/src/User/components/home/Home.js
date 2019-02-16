9f6e8ab674777517d21215f8c7a404394eaaeec8
nabeela <nabeela.abidi.08@gmail.com>
2019-02-15 19:59:57 -0800

    minor fix

--- client/src/User/components/home/Home.js
+++ client/src/User/components/home/Home.js
@@ -16,36 +16,9 @@ class Home extends Component {
     return (
       <div>
         <div className="container">
-          <div class="jumbotron">
+          <div className="jumbotron">
             <h1>Home</h1>
             <p>Welcome, {user.firstName}</p>
-            {/* <div class="row w-150  m-auto">
-              <div class="col-md-2 offset-md-1 ">
-                <div class="text-info text-center">
-                  <h4>Media Consumption</h4>
-                </div>
-              </div>
-              <div class="col-md-2">
-                <div class="text-danger text-center mt-3">
-                  <h4>Emotion</h4>
-                </div>
-              </div>
-              <div class="col-md-2">
-                <div class="text-success text-center mt-3">
-                  <h4>Personality</h4>
-                </div>
-              </div>
-              <div class="col-md-2">
-                <div class="text-warning text-center mt-3">
-                  <h4>Social Media</h4>
-                </div>
-              </div>
-              <div class="col-md-2">
-                <div class="text-default text-center mt-3">
-                  <h4>Culture</h4>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
