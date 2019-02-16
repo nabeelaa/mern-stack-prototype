ddb6c2fd245cb2d19b78be770c597b61091715ca
nabeela <nabeela.abidi.08@gmail.com>
2019-02-15 20:29:49 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.use("/api/brands", brands);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/index.html"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
